7bf5233a2684a0596172b199c6baf59b6e8af8d7
Michał Sieczkowski <michal@ethworks.io>
2019-11-02 16:48:07 +0100

    Create new Kotlin Android app from Navigation Drawer template in decent-app/

---
+++ decent-app/app/src/main/java/eu/bwbw/decent/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package eu.bwbw.decent.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment 3"
+    }
+    val text: LiveData<String> = _text
+}
