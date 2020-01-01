a5a606b1436f49f1172c6b7a89bf24a52136f9f8
Bartosz Wojciechowski <bartosz347@gmail.com>
2020-01-01 18:03:50 +0100

    add navigation to delivery approval request

--- decent-app/app/src/main/java/eu/bwbw/decent/ui/courier/CourierFragment.kt
+++ decent-app/app/src/main/java/eu/bwbw/decent/ui/courier/CourierFragment.kt
@@ -8,6 +8,9 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.NavDirections
+import androidx.navigation.findNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import eu.bwbw.decent.R
 
 class CourierFragment : Fragment() {
@@ -22,6 +25,13 @@ class CourierFragment : Fragment() {
         courierViewModel =
             ViewModelProviders.of(this).get(CourierViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_courier, container, false)
+
+        val fab: FloatingActionButton = root.findViewById(R.id.fab_request_approval)
+        fab.setOnClickListener { view ->
+            val directions: NavDirections = CourierFragmentDirections.actionCourierFragmentToDeliveryApprovalRequest()
+            view.findNavController().navigate(directions)
+        }
+
         val textView: TextView = root.findViewById(R.id.text_courier)
         courierViewModel.text.observe(this, Observer {
             textView.text = it
