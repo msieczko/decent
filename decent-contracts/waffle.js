67b49f6340f250f4956b7b45f4c9b1d35d89bada
Michał Sieczkowski <michal@ethworks.io>
2020-01-04 17:10:25 +0100

    Update Waffle config after ethereum-waffle upgrade

--- decent-contracts/waffle.js
+++ decent-contracts/waffle.js
@@ -1,13 +1,6 @@
-const fs = require('fs');
-
-const getConfig = () => {
-  fs.mkdirSync("build/contracts", {recursive: true});
-  return ({
-    sourcesPath: "src",
-    targetPath: "build/contracts",
-    compiler: "solcjs",
-    solcVersion: "v0.5.7+commit.6da8b019",
-  });
+module.exports = {
+  sourcesPath: "src",
+  targetPath: "build/contracts",
+  compiler: "solcjs",
+  solcVersion: "v0.5.7+commit.6da8b019",
 };
-
-module.exports = getConfig();
